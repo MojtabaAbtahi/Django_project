0db8acc3083f66297cd30696ffaf00fba5395196
MojtabaAbtahi <36766550+MojtabaAbtahi@users.noreply.github.com>
2022-02-01 20:17:37 +0330

    second config

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
+
+app_name = 'app'
 
 urlpatterns = [
-
+    path(r'^$', views.index , name = 'index')
 ]
--- exmp/urls.py
+++ exmp/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('app.urls'))
+    path('app/', include('app.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
